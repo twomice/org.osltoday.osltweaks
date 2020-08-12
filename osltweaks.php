907ac02173f6e3f8c265c019ca958f88667adfd5
Lucky <lucky091588@gmail.com>
2020-08-13 04:55:43 +0800

    Refactor code base on PR comments for osltweaks.php

--- osltweaks.php
+++ osltweaks.php
@@ -20,28 +20,26 @@ function osltweaks_civicrm_buildForm($formName, &$form) {
     // Array of Contribution page id for the Country and State rewrite
     $settings = CRM_Core_BAO_Setting::getItem(NULL, 'com.joineryhq.osltweaks');
 
-    foreach ($settings['page_ids'] as $key => $id) {
-      // Check if contribution page id is match on the $settings and make changes
-      if ($id == $_GET['id']) {
-        if ( $form->elementExists( 'country-1' ) ) {
-          // Remove other Country and show only US
-          $elCountry = & $form->getElement('country-1');
-          $elCountryOptions = & $elCountry->_options;
-          foreach ($elCountryOptions as $key => $option) {
-            if ( $option['attr']['value'] != 1228 ) {
-              unset($elCountryOptions[$key]);
-            }
+    // Check if contribution page id is match on the $settings and make changes
+    if (in_array($form->_id, $settings['us_only_page_ids'])) {
+      if ( $form->elementExists( 'country-1' ) ) {
+        // Remove other Country and show only US
+        $elCountry = $form->getElement('country-1');
+        $elCountryOptions = & $elCountry->_options;
+        foreach ($elCountryOptions as $key => $option) {
+          if ( $option['attr']['value'] != 1228 ) {
+            unset($elCountryOptions[$key]);
           }
+        }
 
-          // Rewrite State/Country label to State
-          $elState = & $form->getElement('state_province-1');
-          $elState->_label = 'State';
+        // Rewrite State/Country label to State
+        $elState = $form->getElement('state_province-1');
+        $elState->_label = 'State';
 
-          // Set Country to US and delete value of State if its US Contribution and User Country is not
-          if ($elCountry->_values[0] != 1228) {
-            $elCountry->_values[0] = 1228;
-            unset($elState->_values[0]);
-          }
+        // Set Country to US and delete value of State if its US Contribution and User Country is not
+        if ($elCountry->_values[0] != 1228) {
+          $elCountry->_values[0] = 1228;
+          unset($elState->_values[0]);
         }
       }
     }
